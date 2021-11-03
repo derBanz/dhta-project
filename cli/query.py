b30cd86e8c4cf740201d89e1d2a04c7129e35a8b
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-11-03 19:43:46 +0100

    Function update incl. infinite warehouse support

--- cli/query.py
+++ cli/query.py
@@ -28,25 +28,107 @@ parser.add_argument(
     help="Name of User (optional)"
 )
 
-# Converting stock to usable lists
-warehouses = [[], []]
-for item in stock:
-    if item['warehouse'] == 1:
-        warehouses[0].append(item)
+
+# Warehouse class
+class Warehouse:
+    def __init__(self, number):
+        self.stock = list()
+        self.number = number
+
+    def __iter__(self):
+        return iter(self.stock)
+
+    def __len__(self):
+        return len(self.stock)
+
+    def get_items(self):
+        return self.stock
+
+    def add_item(self, item):
+        self.stock.append(item)
+
+
+# Browses the warehouses by its categories
+def __browse_categories(warehouses):
+    categories = dict()
+    for wh in warehouses:
+        for item in wh:
+            try:
+                categories[item['category']].append(item)
+            except KeyError:
+                categories[item['category']] = [item]
+    cats = [(k, len(v)) for k, v in reversed(sorted(
+        categories.items(), key=lambda cat: len(cat[1])
+    ))]
+    for i in range(len(cats)):
+        print(f"{i + 1}. {cats[i][0]} ({cats[i][1]})")
+    while True:
+        cat = input("Type the number of the category to browse: ")
+        try:
+            print(f"List of {cats[int(cat)][0]}s available:")
+            break
+        except TypeError:
+            print("Please only type the category number.")
+        except IndexError:
+            print("Please select one of the displayed numbers.")
+    for item in [
+        x for x in sorted(
+            categories[cats[int(cat)][0]],
+            key=lambda x: x['warehouse']
+        )
+    ]:
+        print(
+            f"{item['state']} {item['category']},",
+            f"Warehouse {item['warehouse']}",
+        )
+
+
+# Browses the warehouses for one item
+def __browse_items(warehouses):
+    wish = input("What is the name of the item? ")
+    items = __search_item(warehouses, wish)
+    amount = [len(items[i]) for i in range(len(warehouses))]
+    total_amount = sum(amount)
+    print(f"Amount available: {total_amount}")
+    if len(amount) - amount.count(0) > 1:
+        print("Location:")
+        for i in range(len(amount)):
+            if amount[i] != 0:
+                for item in [
+                    x for x in sorted(
+                        items[i],
+                        key=lambda x: dt.now() - dt.fromisoformat(
+                            x['date_of_stock']
+                        )
+                    )
+                ]:
+                    stocked = (
+                        dt.now() - dt.fromisoformat(
+                            item['date_of_stock']
+                        )
+                    ).days
+                    print(f"- Warehouse {i + 1} (in stock for {stocked} days)")
+                print()
+        max_amount = max(amount)
+        if amount.count(max_amount) == 1:
+            print(f"Maximum availability: {max_amount} in Warehouse {amount.index(max_amount) + 1}.")
+        else:
+            print(f"The following Warehouses have the maximum availability of {max_amount} in store:")
+            for i in range(len(amount)):
+                if amount[i] > 0:
+                    print(f"- Warehouse {i + 1}")
+        __order(amount, wish)
+    elif len(amount) - amount.count(0) == 1:
+        for i in range(len(amount)):
+            if amount[i] != 0:
+                print(f"Location: Warehouse {i + 1}")
+        __order(amount, wish)
     else:
-        warehouses[1].append(item)
+        print("Location: Not in stock")
 
-# Get the user name
-if parser.parse_args().name:
-    usr = parser.parse_args().name
-else:
-    usr = input("Please enter your name: ")
 
-# Greet the user
-print(f"Hello, {usr}.")
-
-# Show the menu and ask to pick a choice
-while True:
+# Presents the user's choices
+def __choice(warehouses):
     opt = input(
         "What would you like to do?\n" +
         "1. List items by warehouse.\n" +
@@ -55,123 +137,107 @@ while True:
         "4. Quit.\n" +
         "Type the number of the operation: "
     )
-
-# If they pick 1
     if opt == "1":
-        for i in range(2):
-            print(f"----------\nItems in Warehouse {i + 1}\n----------")
-            for item in set(
-                [f"{x['state']} {x['category']}" for x in warehouses[i]]
-            ):
-                print(f"- {item}")
-        for i in range(2):
-            print(f"Total items in warehouse {i + 1}: {len(warehouses[i])}")
-
-# Else, if they pick 2
+        __print_all(warehouses)
     elif opt == "2":
-        wish = input("What is the name of the item? ")
-        wishes = [[], []]
-        for i in range(2):
-            for x in warehouses[i]:
-                if f"{x['state']} {x['category']}".lower() == wish.lower():
-                    wishes[i].append(x)
-        amount = [len(wishes[0]), len(wishes[1])]
-        max_amount = sum(amount)
-        print(f"Amount available: {max_amount}")
-        if amount[0] and amount[1]:
-            print("Location:")
-            for i in range(2):
-                for item in [
-                        x for x in sorted(
-                            wishes[i],
-                            key=lambda x: dt.now() - dt.fromisoformat(
-                                x['date_of_stock']
-                            )
-                        )
-                ]:
-                    stocked = (
-                        dt.now() - dt.fromisoformat(
-                            item['date_of_stock']
-                        )
-                    ).days
-                    print(f"- Warehouse {i + 1} (in stock for {stocked} days)")
-            if amount[0] > amount[1]:
-                print(f"Maximum availability: {amount[0]} in Warehouse 1.")
-            elif amount[0] < amount[1]:
-                print(f"Maximum availability: {amount[1]} in Warehouse 2.")
-            else:
-                print(f"Both Warehouses have {amount[0]} in store.")
-        elif amount[0]:
-            print("Location: Warehouse 1")
-        elif amount[1]:
-            print("Location: Warehouse 2")
-        else:
-            print("Location: Not in stock")
-            cont = input("Do you wish to perform another operation?(y/n) ")
-            if cont.lower() == "y":
-                continue
-            else:
-                break
-        order = input(f"Would you like to order {wish}?(y/n) ")
-        if order.lower() == "y":
-            while True:
-                try:
-                    order_amount = int(input("How many would you like? "))
-                    break
-                except ValueError:
-                    print("Please enter a number.")
-            if order_amount > max_amount:
-                print(f"-------\nMax order amount is {max_amount}.\n-------")
-                max_order = input(f"Order {max_amount} of '{wish}'?(y/n) ")
-                if max_order:
-                    print(f"{max_amount} of '{wish}' have been ordered.")
-            else:
-                print(f"{order_amount} of '{wish}' have been ordered.")
-
-# Else, if they pick 3
+        __browse_items(warehouses)
     elif opt == "3":
-        categories = dict()
-        for item in stock:
-            try:
-                categories[item['category']].append(item)
-            except KeyError:
-                categories[item['category']] = [item]
-        cats = [(k, len(v)) for k, v in reversed(sorted(
-            categories.items(), key=lambda cat: len(cat[1])
-        ))]
-        for i in range(len(cats)):
-            print(f"{i + 1}. {cats[i][0]} ({cats[i][1]})")
-        while True:
-            cat = input("Type the number of the category to browse: ")
-            try:
-                print(f"List of {cats[int(cat)][0]}s available:")
-                break
-            except TypeError:
-                print("Please only type the category number.")
-            except IndexError:
-                print("Please select one of the displayed numbers.")
-        for item in [
-            x for x in sorted(
-                categories[cats[int(cat)][0]],
-                key=lambda x: x['warehouse']
-            )
-        ]:
-            print(
-                f"{item['state']} {item['category']},",
-                f"Warehouse {item['warehouse']}",
-            )
-
-# Else, if they pick 4
+        __browse_categories(warehouses)
     elif opt == "4":
-        break
-# Else
+        return False
     else:
         print(f"{opt} is not a valid operation.")
-        continue
+        __choice()
+        return
+    return __da_capo()
+
+
+# One time use to build a list of warehouse-items
+def __create_warehouses():
+    warehouses = list()
+    for item in sorted(stock, key=lambda thing: thing['warehouse']):
+        if item['warehouse'] > len(warehouses):
+            warehouses.append(Warehouse(item['warehouse']))
+        warehouses[item['warehouse'] - 1].add_item(item)
+    return warehouses
+
+
+# Check if another operation is desired
+def __da_capo():
     cont = input("Do you wish to perform another operation?(y/n) ")
-    if cont.lower() != "y":
-        break
+    if cont == "y":
+        return True
+    return False
 
 
-# Thank the user for the visit
-print(f"Thank you for your visit, {usr}.")
+# Say goodbye to the user
+def __goodbye(usr):
+    print(f"Thank you for your visit, {usr}.")
+
+
+# Greet the user
+def __hello():
+    if parser.parse_args().name:
+        usr = parser.parse_args().name
+    else:
+        usr = input("Please enter your name: ")
+    print(f"Hello, {usr}.")
+    return usr
+
+
+# Given the requested item and the listed availibilities, order some of the
+# items
+def __order(amount, wish):
+    order = input(f"Would you like to order {wish}?(y/n) ")
+    if order.lower() == "y":
+        max_amount = sum(amount)
+        while True:
+            try:
+                order_amount = int(input("How many would you like? "))
+                break
+            except ValueError:
+                print("Please enter a number.")
+        if order_amount > max_amount:
+            print(f"-------\nMax order amount is {max_amount}.\n-------")
+            max_order = input(f"Order {max_amount} of '{wish}'?(y/n) ")
+            if max_order.lower() == "y":
+                print(f"{max_amount} of '{wish}' have been ordered.")
+            else:
+                print("Order process aborted.")
+        else:
+            print(f"{order_amount} of '{wish}' have been ordered.")
+
+
+# List all items by warehouse
+def __print_all(warehouses):
+    for i in range(len(warehouses)):
+        print(f"----------\nItems in Warehouse {i + 1}\n----------")
+        for item in set(
+            [f"{x['state']} {x['category']}" for x in warehouses[i]]
+        ):
+            print(f"- {item}")
+    for i in range(len(warehouses)):
+        print(f"Total items in warehouse {i + 1}: {len(warehouses[i])}")
+
+
+# Build a list of items per warehouse that fit the query
+def __search_item(warehouses, wish):
+    wishes = [[] for warehouse in warehouses]
+    for i in range(len(warehouses)):
+        for x in warehouses[i]:
+            if f"{x['state']} {x['category']}".lower() == wish.lower():
+                wishes[i].append(x)
+    return wishes
+
+
+# The active script to be called
+def script():
+    warehouses = __create_warehouses()
+    usr = __hello()
+    da_capo = True
+    while da_capo:
+        da_capo = __choice(warehouses)
+    __goodbye(usr)
+
+
+script()
